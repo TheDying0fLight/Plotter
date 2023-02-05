a9b63b705fe457d3684da89f583e09a9b8af440b
Simon Döhl <simon.doehl@googlemail.com>
2023-02-05 16:08:14 +0100

    Grey Background and Refactor Window Resize Problem

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,78 +14,87 @@ import scalafx.scene.input.MouseEvent
 import scalafx.stage.{WindowEvent, StageStyle}
 import scalafx.Includes._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object WindowFx extends JFXApp3 {
 
-  override def start(): Unit = {
-    val state = ObjectProperty(State(List()))
-    val frame = IntegerProperty(0)
+  var anchorPt: Point2D = null
+  var drag = new Point2D(0,0)
+  var center = new Point2D(0,0)
+  var centerRec = new Point2D(0,0)
+  var thickness = 2
 
-    stage = new JFXApp3.PrimaryStage {
-      width = 600
-      height = 600
-      scene = new Scene {
-        fill = White
-        frame.onChange(Platform.runLater {
-          content = state.value.image
-        })
-      }
-    }
-    // Initialize stage to be movable via mouse
-    initPan()
-    loop(() => frame.update(frame.value + 1))
+  def xAxis(yV: Double, color: Color) = new Rectangle {
+    width = stage.width.value
+    height = thickness
+    x = 0
+    y = yV
+    fill = color
   }
 
-  import scala.concurrent.ExecutionContext.Implicits.global
+  def yAxis(xV: Double, color: Color) = new Rectangle {
+    width = thickness
+    height = stage.height.value
+    x = xV
+    y = 0
+    fill = color
+  }
 
+  def grid = {
+    val xMain = xAxis(centerRec.y, Black)
+    val yMain = yAxis(centerRec.x, Black)
+    var graph = List(xMain,yMain)
+    var temp = center.y%100
+    while (temp <= stage.height.value) {
+      graph :+ xAxis(temp,Blue)
+      temp += 100
+    }
+    graph :+ xAxis(100,Blue)
+    println(graph)
+    graph
+  }
+
+  /*case class State(fs: List[String]) {
+
+    //def evalFunction(func: String): List[Rectangle] =
+    def image(): List[Rectangle] = List(yAxis, xAxis) 
+  }*/
+
+  //Reapeating loop with short wait
   def loop(update: () => Unit): Unit =
     Future {
       update()
       Thread.sleep(1)
     }.flatMap(_ => Future(loop(update)))
 
-  def square(xr: Double, yr: Double, color: Color) = new Rectangle {
-    x = xr
-    y = yr
-    width = 2
-    height = 2
-    fill = color
-  }
-
-  var drag = new Point2D(0, 0)
-
-  def yAxis = new Rectangle {
-    x = (drag.x+stage.width.value)/2
-    y = 0
-    width = 2
-    height = stage.height.value
-    fill = Black
-  }
-
-  def xAxis = new Rectangle {
-    x = 0
-    y = (drag.y+stage.height.value)/2
-    width = stage.width.value
-    height = 2
-    fill = Black
-  }
-
-  case class State(fs: List[String]) {
-
-    //def evalFunction(func: String): List[Rectangle] =
-
-    def image(): List[Rectangle] = List(yAxis, xAxis) 
-  }
-
-  /**
-   * Initialize the stage to allow the mouse cursor to move the application
-   * using dragging.
-   */
-  var anchorPt: Point2D = null
-  def initPan(): Unit = {
+  def initDrag() ={
     val scene = stage.getScene()
     scene.onMousePressed = (event: MouseEvent) => anchorPt = new Point2D(event.screenX, event.screenY)
     scene.onMouseDragged = (event: MouseEvent) => {
-      drag = new Point2D((event.screenX - anchorPt.x)*2 + drag.x, (event.screenY - anchorPt.y)*2 + drag.y)
-      anchorPt = new Point2D(event.screenX, event.screenY)}
+      drag = new Point2D(event.screenX - anchorPt.x + drag.x, event.screenY - anchorPt.y + drag.y)
+      anchorPt = new Point2D(event.screenX, event.screenY)
+      center = new Point2D(stage.width.value/2+drag.x, stage.height.value/2+drag.y)
+      centerRec = new Point2D(stage.width.value/2+drag.x-thickness/2, stage.height.value/2+drag.y-thickness/2)}}
+
+  //Initalization of programm
+  override def start(): Unit = {
+    val frame = IntegerProperty(0)
+    //val state = ObjectProperty(State(List()))
+
+    //Graphical Window of Application
+    stage = new JFXApp3.PrimaryStage {
+      width = 1280
+      height = 720
+      scene = new Scene {
+        title = "Plotter"
+        fill = LightGrey
+        frame.onChange(Platform.runLater {content = grid})
+      }
+    }
+    center = new Point2D(stage.width.value/2+drag.x, stage.height.value/2+drag.y)
+    centerRec = new Point2D(stage.width.value/2+drag.x-thickness/2, stage.height.value/2+drag.y-thickness/2)
+    initDrag()
+    //loop to update image
+    loop(() => frame.update(frame.value + 1))
   }
 }
